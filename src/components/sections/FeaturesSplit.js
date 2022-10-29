87df61824c3210fbfc4d58a50165902d9ca6466d
Anirudh A V <anirudh.av02@gmail.com>
2022-10-29 19:53:37 +0530

    <p> tag at line 127 changed to <div> tag.
    <p> tag cannot have <ul> as its child.

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -68,13 +68,13 @@ const FeaturesSplit = ({
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Lightning fast workflow
-                  </div>
+                </div>
                 <h3 className="mt-0 mb-12">
                   Data-driven insights
-                  </h3>
+                </h3>
                 <p className="m-0">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                </p>
               </div>
               <div className={
                 classNames(
@@ -95,12 +95,12 @@ const FeaturesSplit = ({
                 {/* <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   Lightning fast workflow
                 </div> */}
-                  <h3 className="mt-0 mb-12">
-                    ABOUT IEDC
-                    </h3>
+                <h3 className="mt-0 mb-12">
+                  ABOUT IEDC
+                </h3>
                 <p className="m-0">
-                The Innovation and Entrepreneurship Development Cell (IEDC) is an active student-run cell under CET Centre for Interdisciplinary Research (CCIR) that seeks to create and promote innovation and entrepreneurship skills among the students of CET. The cell works in close association with Kerala Startup Mission, CET –TBI and other student clubs. It envisions fostering innovative thinking and becoming a facilitator between the students inside the campus and the industry outside by acting as a hub of contacts and network that helps in finding the wannabe entrepreneurs the right people to approach for any task.
-                  </p>
+                  The Innovation and Entrepreneurship Development Cell (IEDC) is an active student-run cell under CET Centre for Interdisciplinary Research (CCIR) that seeks to create and promote innovation and entrepreneurship skills among the students of CET. The cell works in close association with Kerala Startup Mission, CET –TBI and other student clubs. It envisions fostering innovative thinking and becoming a facilitator between the students inside the campus and the industry outside by acting as a hub of contacts and network that helps in finding the wannabe entrepreneurs the right people to approach for any task.
+                </p>
               </div>
               <div className={
                 classNames(
@@ -119,17 +119,17 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  </div>
+                </div>
                 <h3 className="mt-0 mb-12">
                   OBJECTIVES
-                  </h3>
-                <p className="m-0">
+                </h3>
+                <div className="m-0">
                   <ul>
-                  <li>To motivate the students to think creatively and generate innovative ideas from all fields of engineering.</li>
-                  <li>To transform innovative ideas into socially and industrially relevent products.</li>
-                  <li>To inculcate a culture of innovation driven entrepreneurship.</li>
+                    <li>To motivate the students to think creatively and generate innovative ideas from all fields of engineering.</li>
+                    <li>To transform innovative ideas into socially and industrially relevent products.</li>
+                    <li>To inculcate a culture of innovation driven entrepreneurship.</li>
                   </ul>
-                  </p>
+                </div>
               </div>
               <div className={
                 classNames(
