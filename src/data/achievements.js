736e9b54c9670da4cef85ebc2d575d2e2bedbd86
jaidan22 <jaidansleeba22@gmail.com>
2022-11-03 21:56:48 +0530

    added achievements section (swiper.js)

---
+++ src/data/achievements.js
@@ -0,0 +1,59 @@
+import patent2 from "../assets/images/achievements/patent2.png"
+import patent from "../assets/images/achievements/patent.jpg"
+import mhrd from "../assets/images/achievements/mhrd.jpg"
+import vguard from "../assets/images/achievements/vguard.jpg"
+import ket from "../assets/images/achievements/ket.jpg"
+const achievements = [
+  {
+    title: "Patent Grant",
+    desc: "Congratulations to Dr Rani S, Dr Muhammed Arif, Sneha Edla and Ananthan D Thampi on being successfully granted patent for developing a green cutting fluid that has the potential to reduce the country's dependence on imported cutting fluid.",
+    img: patent2,
+    year: "2020",
+  },
+  {
+    title: "Patent Grant",
+    desc: "Congratulations to Prof. Reshmi Bhooshan (faculty mentor), Praveen Sekhar (2014 AEI),\
+        and Lia Joseph (2014 AEI)on being successfully granted patent for the invention titled\
+        “An Automated duct fan-based Wall Climbing Device”.",
+    img: patent,
+    year: "2020",
+  },
+  {
+    title: "MHRD National Innovation Fest 2019",
+    desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME were finalists at\
+        National Innovation Fest 2019 hosted by MHRD Govt. of India.",
+    img: mhrd,
+    year: "2019",
+  },
+  {
+    title: "V-Guard Big Idea Challenge 2019",
+    desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME bagged the first\
+        runner up position in the V-Guard Big Idea Challenge conducted in 2019.",
+    img: vguard,
+    year: "2019",
+  },
+  {
+    title: "Ketcon 2019",
+    desc: "Amal C Shaji, Akshay Vijayan, Devika Ramgopal secured first prize in paper presentation\
+        in the track 'Developments in Computer Applications' conducted as a part of KTU Techfest\
+        2019.",
+    img: ket,
+    year: "2019",
+  },
+  // {
+  //     title: "",
+  //     desc: "",
+  //     img:""
+  // },
+  // {
+  //     title: "",
+  //     desc: "",
+  //     img:""
+  // },
+  // {
+  //     title: "",
+  //     desc: "",
+  //     img:""
+  // },
+];
+export default achievements;
