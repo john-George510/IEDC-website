c4776a244936078be7af25893236445ec6d7413a
Anirudh A V <anirudh.av02@gmail.com>
2022-11-10 19:44:03 +0530

    Tailwind CSS configured

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import './index.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
