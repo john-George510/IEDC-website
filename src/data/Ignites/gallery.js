bcf6a1f5bb48d316ea998981474982b7ba3cc69e
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 23:45:35 +0530

    Ignites: gallery data configured

---
+++ src/data/Ignites/gallery.js
@@ -0,0 +1,55 @@
+import { Ig1, Ig2, Ig3, Ig4, Ig5, Ig6, Ig7, Ig8, Ig9, Ig10, Ig11, Ig12 } from '../../assets/images/Ignites/Gallery';
+
+const data = [
+    {
+        img: Ig12,
+        text: 'Ignites 12',
+    },
+    {
+        img: Ig1,
+        text: 'Ignites 1',
+    },
+    {
+        img: Ig11,
+        text: 'Ignites 11',
+    },
+    {
+        img: Ig2,
+        text: 'Ignites 2',
+    },
+    {
+        img: Ig6,
+        text: 'Ignites 6',
+    },
+    {
+        img: Ig3,
+        text: 'Ignites 3',
+    },
+    // {
+    //     img: Ig4,
+    //     text: 'Ignites 4',
+    // },
+    {
+        img: Ig5,
+        text: 'Ignites 5',
+    },
+    {
+        img: Ig7,
+        text: 'Ignites 7',
+    },
+    // {
+    //     img: Ig8,
+    //     text: 'Ignites 8',
+    // },
+    // {
+    //     img: Ig9,
+    //     text: 'Ignites 9',
+    // },
+    {
+        img: Ig10,
+        text: 'Ignites 10',
+    },
+
+];
+
+export default data;
