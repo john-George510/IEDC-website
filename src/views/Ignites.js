110f87587adc1a4a8aa550c38839f9c55ca062a1
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 23:50:56 +0530

    Ignites: Page first draft complete
    To do:
    All data need to be entered into the page
    Relevant images need to be added
    Page needs to be proofread
    Navigation needs to be added

--- src/views/Ignites.js
+++ src/views/Ignites.js
@@ -1,57 +1,22 @@
-import React from 'react'
+import Hero from "../components/ignites/Hero";
 import Background from '../components/layout/partials/Background'
-import classNames from 'classnames';
-import { SectionProps } from '../utils/SectionProps';
-import './styles.css';
+import Gallery from "../components/ignites/Gallery";
+import Details from "../components/ignites/Details";
+import Events from "../components/ignites/Events";
 
-const Ignites = ({
-    className,
-    topOuterDivider,
-    bottomOuterDivider,
-    topDivider,
-    bottomDivider,
-    hasBgColor,
-    invertColor,
-    ...props
-}) => {
-
-    const outerClasses = classNames(
-        'hero section center-content',
-        topOuterDivider && 'has-top-divider',
-        bottomOuterDivider && 'has-bottom-divider',
-        hasBgColor && 'has-bg-color',
-        invertColor && 'invert-color',
-        className
-    );
-
-    const innerClasses = classNames(
-        'hero-inner section-inner',
-        topDivider && 'has-top-divider',
-        bottomDivider && 'has-bottom-divider'
-    );
-
+const Ignites = () => {
     return (
         <>
-            <section
-                {...props}
-                className={outerClasses}
-            >
-                <div className="container-sm">
-                    <div className={innerClasses}>
-                        {/* <div className="hero-content"> */}
-                            <div className='flex justify-center items-center'>
-                                <h1 className="ignites-main-text mt-0 mb-16 reveal-from-bottom z-10 para text-6xl text-slate-200" data-reveal-delay="200">ignit<span className='text-color-primary'>ES '22</span></h1>
-                                <Background />
-                            </div>
-                        {/* </div> */}
-                        <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-                            <h2 className="ignites-sub-text mt-0 mb-16 reveal-from-bottom z-10 para text-3xl text-slate-400" data-reveal-delay="600">Coming soon</h2>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Hero className="illustration-section-01" />
+            <Details invertMobile
+                imageFill
+                className="illustration-section-02"
+            />
+            <Events />
+            <Gallery />
+            <Background className='-z-10' />
         </>
-    )
-}
+    );
+};
 
-export default Ignites
+export default Ignites;
