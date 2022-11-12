9892944937a4ace1391b3ffc15885206cb942b5a
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 23:48:59 +0530

    Upcoming events data formatted

--- src/data/upcomingevents.js
+++ src/data/upcomingevents.js
@@ -7,60 +7,60 @@ const upcomingevents = [
     title: "Sales and Marketing Workshop",
     date: "01/10/2022",
     time: "06:30pm",
-    venue:"Gmeet",
+    venue: "Gmeet",
     img: marketing,
   },
   {
     title: "Get To Know Python",
     date: "04/09/2022",
     time: "06:00pm",
-    venue:"Gmeet",
+    venue: "Gmeet",
     img: python,
-  },
-  {
-  title: "Sales and Marketing Workshop",
-  date: "01/10/2022",
-  time: "06:30pm",
-  venue:"Gmeet",
-  img: marketing,
   },
   {
     title: "Sales and Marketing Workshop",
     date: "01/10/2022",
     time: "06:30pm",
-    venue:"Gmeet",
+    venue: "Gmeet",
     img: marketing,
-    },
-    {
-      title: "Sales and Marketing Workshop",
-      date: "01/10/2022",
-      time: "06:30pm",
-      venue:"Gmeet",
-      img: marketing,
-      },
+  },
+  {
+    title: "Sales and Marketing Workshop",
+    date: "01/10/2022",
+    time: "06:30pm",
+    venue: "Gmeet",
+    img: marketing,
+  },
+  {
+    title: "Sales and Marketing Workshop",
+    date: "01/10/2022",
+    time: "06:30pm",
+    venue: "Gmeet",
+    img: marketing,
+  },
 
-//   {
-//     title: "MHRD National Innovation Fest 2019",
-//     desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME were finalists at\
-//         National Innovation Fest 2019 hosted by MHRD Govt. of India.",
-//     img: mhrd,
-//     year: "2019",
-//   },
-//   {
-//     title: "V-Guard Big Idea Challenge 2019",
-//     desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME bagged the first\
-//         runner up position in the V-Guard Big Idea Challenge conducted in 2019.",
-//     img: vguard,
-//     year: "2019",
-//   },
-//   {
-//     title: "Ketcon 2019",
-//     desc: "Amal C Shaji, Akshay Vijayan, Devika Ramgopal secured first prize in paper presentation\
-//         in the track 'Developments in Computer Applications' conducted as a part of KTU Techfest\
-//         2019.",
-//     img: ket,
-//     year: "2019",
-//   },
+  //   {
+  //     title: "MHRD National Innovation Fest 2019",
+  //     desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME were finalists at\
+  //         National Innovation Fest 2019 hosted by MHRD Govt. of India.",
+  //     img: mhrd,
+  //     year: "2019",
+  //   },
+  //   {
+  //     title: "V-Guard Big Idea Challenge 2019",
+  //     desc: "Jithin Sankar S5EE, Vishnu vikraman S5EE and Akshay Krishna R S5ME bagged the first\
+  //         runner up position in the V-Guard Big Idea Challenge conducted in 2019.",
+  //     img: vguard,
+  //     year: "2019",
+  //   },
+  //   {
+  //     title: "Ketcon 2019",
+  //     desc: "Amal C Shaji, Akshay Vijayan, Devika Ramgopal secured first prize in paper presentation\
+  //         in the track 'Developments in Computer Applications' conducted as a part of KTU Techfest\
+  //         2019.",
+  //     img: ket,
+  //     year: "2019",
+  //   },
   // {
   //     title: "",
   //     desc: "",
