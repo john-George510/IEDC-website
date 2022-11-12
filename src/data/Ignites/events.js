d69fd12f7638fc6f5af9beec15dea1df0534bcac
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 23:46:47 +0530

    Ignites: upcomming events data configured

---
+++ src/data/Ignites/events.js
@@ -0,0 +1,30 @@
+import Ig6 from '../../assets/images/Ignites/Events/Ig6.jpg';
+import python  from '../../assets/images/Ignites/Events/python.png';
+import marketing from '../../assets/images/Ignites/Events/marketing.png';
+
+const upcomingevents = [
+    {
+        title: "Ignites 2021",
+        date: "2022-12-05",
+        time: "06:30pm",
+        venue: "Gmeet",
+        img: Ig6,
+    },
+    {
+        title: "Sales and Marketing Workshop",
+        date: "2022-12-12",
+        time: "06:30pm",
+        venue: "Gmeet",
+        img: marketing,
+    },
+    {
+        title: "Python Workshop",
+        date: "2022-11-05",
+        time: "06:30pm",
+        venue: "Gmeet",
+        img: python,
+    },
+];
+
+export default upcomingevents;
+
