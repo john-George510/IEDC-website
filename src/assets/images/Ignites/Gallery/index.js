b3d713e0d3d9a2cabc4dc734908f6095529bc3c0
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 23:48:17 +0530

    Ignites: Gallery imports

---
+++ src/assets/images/Ignites/Gallery/index.js
@@ -0,0 +1,12 @@
+export { default as Ig1 } from './Ig1.JPG';
+export { default as Ig2 } from './Ig2.JPG';
+export { default as Ig3 } from './Ig3.JPG';
+export { default as Ig4 } from './Ig4.JPG';
+export { default as Ig5 } from './Ig5.JPG';
+export { default as Ig6 } from './Ig6.jpg';
+export { default as Ig7 } from './Ig7.jpg';
+export { default as Ig8 } from './Ig8.png';
+export { default as Ig9 } from './Ig9.JPG';
+export { default as Ig10 } from './Ig10.JPG';
+export { default as Ig11 } from './Ig11.jpg';
+export { default as Ig12 } from './Ig12.JPG';
